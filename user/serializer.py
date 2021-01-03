c4dfd5f8d1a979bc45116f4849bf9893e92659b1
selcuk.akkurt <selcuk.akkurt@intelprobe.com.tr>
2021-01-03 16:58:56 +0300

    If user group is not 'admin' then user can't change user group.

--- user/serializer.py
+++ user/serializer.py
@@ -1,10 +1,19 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, CurrentUserDefault
 from user.models import User
 
 
 class UserSerializer(ModelSerializer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if 'view' in self.context and self.context['view'].action in ['update']:
+
+            # Kullanıcı eğer admin statüsünde değilse group bilgisini set edemez
+            if self.context['request'].user.groups.name != "admin":
+                self.fields.pop('groups', None)
+
     class Meta:
-        fields = ('id', 'first_name', 'last_name', 'username', 'groups', 'email')
+        fields = ('id', 'first_name', 'last_name', 'username', 'email', 'groups')
+        # read_only_fields = ('groups',)
         model = User
         extra_kwargs = {'password': {'write_only': True}}
 
